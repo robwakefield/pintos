1322aeb7727c718d7531f1766f4f27c2f2735739
rob <rgw20@ic.ac.uk>
2022-10-27 17:08:22 +0100

    Fix implementation of floating point functions to use F=(1<<14)

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -1,13 +1,15 @@
-#define FP(n) (n * 14)
+#define FP_F (1 << 14)
 
-#define FP_FLOOR(x) (x / 14)
+#define FP(n) (n * FP_F)
 
-#define FP_ROUND(x) (x >= 0 ? (x + (14 / 2)) / 14 : (x - (14 / 2)) / 14) 
+#define FP_FLOOR(x) (x / FP_F)
+
+#define FP_ROUND(x) (x >= 0 ? (x + (FP_F / 2)) / FP_F : (x - (FP_F / 2)) / FP_F) 
 
 #define FP_ADD(x, y) (x + y)
 
 #define FP_SUB(x, y) (x - y)
 
-#define FP_MUL(x, y) ((((int64_t) x) * y) / 14)
+#define FP_MUL(x, y) ((((int64_t) x) * y) / FP_F)
 
-#define FP_DIV(x, y) ((((int64_t) x) * 14) / y)
+#define FP_DIV(x, y) ((((int64_t) x) * FP_F) / y)
