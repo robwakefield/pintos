dd6936581ca4899b6b3efc065536ea61af93f5b4
Nada Struharova <ns2321@ic.ac.uk>
2022-11-08 09:19:51 +0000

    Created enumeration and a function pointer array for system calls.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,16 +5,118 @@
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
+static int (*syscall_handlers[20]) (struct intr_frame *);     /* Array of function pointers so syscall handlers. */
+
+/* System call handler functions. */
+// do these need to be static or not?
+void syscall_halt (struct intr_frame *);
+void syscall_exit (struct intr_frame *);
+void syscall_exec (struct intr_frame *);
+void syscall_wait (struct intr_frame *);
+void syscall_create (struct intr_frame *);
+void syscall_remove (struct intr_frame *);
+void syscall_open (struct intr_frame *);
+void syscall_filesize (struct intr_frame *);
+void syscall_read (struct intr_frame *);
+void syscall_write (struct intr_frame *);
+void syscall_seek (struct intr_frame *);
+void syscall_tell (struct intr_frame *);
+void syscall_close (struct intr_frame *);
 
 void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  
+  syscall_handlers[SYS_HALT] = &syscall_halt;
+  syscall_handlers[SYS_EXIT] = &syscall_exit;
+  syscall_handlers[SYS_EXEC] = &syscall_exec;
+  syscall_handlers[SYS_WAIT] = &syscall_wait;
+  syscall_handlers[SYS_CREATE] = &syscall_create;
+  syscall_handlers[SYS_REMOVE] = &syscall_remove;
+  syscall_handlers[SYS_OPEN] = &syscall_open;
+  syscall_handlers[SYS_FILESIZE] = &syscall_filesize;
+  syscall_handlers[SYS_READ] = &syscall_read;
+  syscall_handlers[SYS_WRITE] = &syscall_write;
+  syscall_handlers[SYS_SEEK] = &syscall_seek;
+  syscall_handlers[SYS_TELL] = &syscall_tell;
+  syscall_handlers[SYS_CLOSE] = &syscall_close;
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
   printf ("system call!\n");
+
+  /* check if pointer is valid */
+
+  /* get system call number */
+  // int syscall_num = *f->esp;
+
+  /* call syscall from array syscall_handlers */
+  // syscall_handlers[syscall_num](f);
+
+
   thread_exit ();
+}
+
+/* Implement all syscalls needed for Task 2 - User Programs */
+void
+syscall_halt(struct intr_frame *f) {
+
+}
+
+void
+syscall_exit(struct intr_frame *f) {
+  
+}
+
+void
+syscall_exec(struct intr_frame *f) {
+
+}
+
+void
+syscall_create(struct intr_frame *f) {
+  
+}
+
+void
+syscall_remove(struct intr_frame *f) {
+  
+}
+
+void
+syscall_open(struct intr_frame *f) {
+  
+}
+
+void
+syscall_filesize(struct intr_frame *f) {
+  
+}
+
+void
+syscall_read(struct intr_frame *f) {
+  
+}
+
+void
+syscall_write(struct intr_frame *f) {
+  
+}
+
+void
+syscall_seek(struct intr_frame *f) {
+  
+}
+
+void
+syscall_tell(struct intr_frame *f) {
+  
+}
+
+void
+syscall_close(struct intr_frame *f) {
+  
 }
