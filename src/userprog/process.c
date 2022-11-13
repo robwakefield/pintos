65c032ae72a2ad76390f48a8e10b5afabaea4fe1
rob <rgw20@ic.ac.uk>
2022-11-13 22:25:31 +0000

    Set up the stack

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -18,14 +18,15 @@
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 
-static thread_func start_process NO_RETURN;
-static bool load (const char *cmdline, void (**eip) (void), void **esp);
-
 /* Passed argument struct */
 struct arguments {
   int argc;
-  char *argv[128];
+  char *argv[128]; // TODO: limit lemgth?
 };
+
+static thread_func start_process NO_RETURN;
+static bool load (const struct arguments *args, void (**eip) (void), void **esp);
+static void *push_args_on_stack (const struct arguments *args);
 
 /* Starts a new thread running a user program loaded from
    FILENAME.  The new thread may be scheduled (and may even exit)
@@ -74,7 +75,6 @@ static void
 start_process (void *aux)
 {
   struct arguments *args = (struct arguments *) aux;
-  char* file_name = args->argv[0]; // TODO: check if argv is empty
   struct intr_frame if_;
   bool success;
 
@@ -83,10 +83,10 @@ start_process (void *aux)
   if_.gs = if_.fs = if_.es = if_.ds = if_.ss = SEL_UDSEG;
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
-  success = load (file_name, &if_.eip, &if_.esp);
+  success = load (args, &if_.eip, &if_.esp);
 
   /* If load failed, quit. */
-  palloc_free_page (file_name);
+  palloc_free_page (args->argv[0]);
   if (!success) 
     thread_exit ();
 
@@ -221,7 +221,7 @@ struct Elf32_Phdr
 #define PF_W 2          /* Writable. */
 #define PF_R 4          /* Readable. */
 
-static bool setup_stack (void **esp);
+static bool setup_stack (const struct arguments *args, void **esp);
 static bool validate_segment (const struct Elf32_Phdr *, struct file *);
 static bool load_segment (struct file *file, off_t ofs, uint8_t *upage,
                           uint32_t read_bytes, uint32_t zero_bytes,
@@ -232,8 +232,12 @@ static bool load_segment (struct file *file, off_t ofs, uint8_t *upage,
    and its initial stack pointer into *ESP.
    Returns true if successful, false otherwise. */
 bool
-load (const char *file_name, void (**eip) (void), void **esp) 
+load (const struct arguments *args, void (**eip) (void), void **esp) 
 {
+  if (args->argc == 0) {
+    return TID_ERROR; // TODO: is this correct behaviour?
+  }
+  char *file_name = args->argv[0]; 
   struct thread *t = thread_current ();
   struct Elf32_Ehdr ehdr;
   struct file *file = NULL;
@@ -328,7 +332,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
     }
 
   /* Set up stack. */
-  if (!setup_stack (esp))
+  if (!setup_stack (args, esp))
     goto done;
 
   /* Start address. */
@@ -467,7 +471,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
 /* Create a minimal stack by mapping a zeroed page at the top of
    user virtual memory. */
 static bool
-setup_stack (void **esp) 
+setup_stack (const struct arguments *args, void **esp) 
 {
   uint8_t *kpage;
   bool success = false;
@@ -476,12 +480,53 @@ setup_stack (void **esp)
   if (kpage != NULL) 
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
-      if (success)
-        *esp = PHYS_BASE - 12;
-      else
+      if (success) {
+        printf ("pushing args\n");
+        *esp = push_args_on_stack (args);
+        printf ("Set up stack:\n");
+        hex_dump (PHYS_BASE - 64, kpage + PGSIZE - 64, 64, true);
+      } else {
         palloc_free_page (kpage);
+      }
     }
   return success;
+}
+
+static void *push_args_on_stack (const struct arguments *args) {
+  void *esp = PHYS_BASE;
+  char *arg_pointer[args->argc];
+  arg_pointer[args->argc] = 0; /* NULL pointer sentinel */
+  
+  /* Push arguments on to stack */
+  for (int i = args->argc - 1; i >= 0; i--) {
+    size_t len = strlen (args->argv[i]) + 1;
+    esp -= len;
+    strlcpy (esp, args->argv[i], len);
+    arg_pointer[i] = esp;
+  }
+  
+  /* Round esp down to multiple of 4 */
+  esp -= 1; // TODO: change this
+  memset (esp, 0, 1);
+
+  /* Push address of each argument (RTL) */
+  for (int i = args->argc; i >= 0; i--) {
+    esp -= 4; // TODO: change to sizeof?
+    memcpy (esp, &arg_pointer[i], 4);
+  }
+  
+  /* Push argv */
+  memcpy (esp - 4, &esp, 4);
+  esp -= 4;
+  /* Push argc */
+  esp -= 4;
+  memcpy (esp, &args->argc, 4);
+  printf("done\n");
+  /* Push a fake return address */
+  esp -=4;
+  memset (esp, 0, 4); // TODO: uneccessary?
+  
+  return esp;
 }
 
 /* Adds a mapping from user virtual address UPAGE to kernel
