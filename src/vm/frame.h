6544cb52d0c29e2bd8e14c6a345c194a82d24562
Nada Struharova <ns2321@ic.ac.uk>
2022-11-24 15:53:18 +0000

    Create struct for frame entry into the frame table.

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -7,12 +7,13 @@
 
 struct frame_entry
 {
-    
+    void *frame_address;
+    struct thread *owner;
     struct hash_elem elem;
 };
 
 void init_frame_table(void);
 
-unsigned frame_hash ();
+unsigned frame_hash (struct hash *elem);
 
 bool frame_less_func ();
