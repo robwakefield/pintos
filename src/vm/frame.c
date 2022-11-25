1f847fad76e600b73cc7c7767a9336ba44b181bb
rob <rgw20@ic.ac.uk>
2022-11-25 12:21:19 +0000

    Remove nested locking from frame.c. Add correct malloc-ing in search_elem()

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -10,17 +10,13 @@ void frame_table_init (void) {
 }
 
 unsigned frame_hash (const struct hash_elem *e, void *aux UNUSED) {
-  lock_acquire (&frame_table_lock);
   struct frame_entry *entry = hash_entry (e, struct frame_entry, elem);
-  lock_release (&frame_table_lock);
   return ((int) entry->frame_address / PGSIZE) % BUCKET_COUNT;
 }
 
 bool frame_less_func (const struct hash_elem *e1, const struct hash_elem *e2, void *aux UNUSED) {
-  lock_acquire (&frame_table_lock);
   struct frame_entry *a = hash_entry (e1, struct frame_entry, elem);
   struct frame_entry *b = hash_entry (e2, struct frame_entry, elem);
-  lock_release (&frame_table_lock);
   return a->frame_address < b->frame_address;
 }
 
@@ -31,10 +27,11 @@ struct frame_entry *create_entry (void *frame) {
   return f;
 }
 
-struct hash_elem *search_elem (void *address) {
+struct hash_entry *search_elem (void *address) {
   struct frame_entry *temp_entry;
+  temp_entry = malloc (sizeof (struct frame_entry));
   temp_entry->frame_address = address;
-  return &temp_entry->elem;
+  return temp_entry;
 }
 
 void add_frame (void *frame) {
@@ -46,7 +43,9 @@ void add_frame (void *frame) {
 
 void remove_frame (void *frame) {
   lock_acquire (&frame_table_lock);
-  hash_delete (frame_table, search_elem (frame)); 
+  struct frame_entry *temp_entry = search_elem (frame);
+  hash_delete (frame_table, &temp_entry->elem);
+  free (temp_entry); 
   lock_release (&frame_table_lock);
 }
 
