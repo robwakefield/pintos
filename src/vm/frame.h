27b75253de490ca9e6350edefe46d0e2c7d6b847
rob <rgw20@ic.ac.uk>
2022-11-25 12:26:21 +0000

    Add missing prototypes to frame.h

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -21,10 +21,14 @@ struct frame_entry
 void frame_table_init (void);
 
 unsigned frame_hash (const struct hash_elem *h, void *aux UNUSED);
-
 bool frame_less_func (const struct hash_elem *e1, const struct hash_elem *e2, void *aux UNUSED);
 
 void *frame_alloc (enum palloc_flags flags);
 void frame_free (void *frame);
+
+struct frame_entry *create_entry (void *frame);
+struct hash_entry *search_elem (void *address);
+void add_frame (void *frame);
+void remove_frame (void *frame);
 
 #endif
