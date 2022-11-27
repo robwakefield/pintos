e5068c5f263a47a8e74ab920ce839b647f74da95
Nada Struharova <ns2321@ic.ac.uk>
2022-11-27 13:37:28 +0000

    Add commenting around frame table functions, use provided hash_bytes for our hash function.

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -3,62 +3,90 @@
 static struct hash *frame_table;
 static struct lock frame_table_lock;
 
-void frame_table_init (void) {
+/* Initialization code for frame table. */
+void
+frame_table_init (void) {
   lock_init (&frame_table_lock);
   frame_table = malloc (sizeof (struct hash));
   hash_init (frame_table, frame_hash, frame_less_func, NULL);
 }
 
-unsigned frame_hash (const struct hash_elem *e, void *aux UNUSED) {
-  struct frame_entry *entry = hash_entry (e, struct frame_entry, elem);
-  return ((int) entry->frame_address / PGSIZE) % BUCKET_COUNT;
+/* Hash function for frame table. */
+unsigned
+frame_hash (const struct hash_elem *e, void *aux UNUSED) {
+  const struct frame_entry *entry = hash_entry (e, struct frame_entry, elem);
+  return hash_bytes (&entry->frame_address, sizeof (entry->frame_address));
 }
 
-bool frame_less_func (const struct hash_elem *e1, const struct hash_elem *e2, void *aux UNUSED) {
-  struct frame_entry *a = hash_entry (e1, struct frame_entry, elem);
-  struct frame_entry *b = hash_entry (e2, struct frame_entry, elem);
+/* Comparing function for frame table. */
+bool
+frame_less_func (const struct hash_elem *e1, const struct hash_elem *e2, void *aux UNUSED) {
+  const struct frame_entry *a = hash_entry (e1, struct frame_entry, elem);
+  const struct frame_entry *b = hash_entry (e2, struct frame_entry, elem);
   return a->frame_address < b->frame_address;
 }
 
-struct frame_entry *create_entry (void *frame) {
+/* Creates a new frame table entry, which stores the address of the frame, 
+the process that owns the frame. */
+struct frame_entry *
+create_entry (void *frame) {
   struct frame_entry *f = malloc (sizeof (struct frame_entry));
   f->owner = thread_current ();
   f->frame_address = frame;
   return f;
 }
 
-struct frame_entry *search_elem (void *address) {
+/* Creates a "fake" temporary hash element on the stack,
+containing just enough information for hash_find to find the element we are looking for. */
+struct frame_entry *
+search_elem (void *address) {
   struct frame_entry *temp_entry;
   temp_entry = malloc (sizeof (struct frame_entry));
   temp_entry->frame_address = address;
   return temp_entry;
 }
 
-void add_frame (void *frame) {
+/* Adds a given frame table entry to the frame table. */
+void
+add_frame (void *frame) {
   lock_acquire (&frame_table_lock);
+
   struct frame_entry *entry = create_entry (frame); 
   hash_insert (frame_table, &entry->elem);
+
   lock_release (&frame_table_lock);
 }
 
-void remove_frame (void *frame) {
+/* Removes given frame table entry from the frame table. */
+void
+remove_frame (void *frame) {
   lock_acquire (&frame_table_lock);
+
   struct frame_entry *temp_entry = search_elem (frame);
   hash_delete (frame_table, &temp_entry->elem);
+  palloc_free_page (frame->frame_address); // necessary??
   free (temp_entry); 
+
   lock_release (&frame_table_lock);
 }
 
-void *frame_alloc (enum palloc_flags flags) {
-  void *f = palloc_get_page (PAL_USER | flags);
-  if (f == NULL) {
-    ASSERT(false);
+/* Allocates a frame for the given page. */
+void *
+frame_alloc (enum palloc_flags flags) {
+  void *kpage = palloc_get_page (PAL_USER | flags);
+  if (kpage == NULL) {
+    PANIC ("Page eviction not implemented.");
+    /* TODO: evict page & palloc again */
   }
+
   add_frame (f);
   return f;
 }
 
-void frame_free (void *frame) {
+/* Frees all resources used by frame table entry
+and removes entry from the frame table. */
+void
+frame_free (void *frame) {
   palloc_free_page (frame);
   remove_frame (frame);
 }
