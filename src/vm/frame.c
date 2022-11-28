8f128945cd5313d48f6c16584c7217b35e1c0f84
Nada Struharova <ns2321@ic.ac.uk>
2022-11-28 12:25:28 +0000

    Fix warnings and errors, free resources when removing a frame table entry.

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -53,8 +53,9 @@ add_frame (void *frame) {
 
   struct frame_entry *entry = create_entry (frame); 
   hash_insert (frame_table, &entry->elem);
-
+  
   lock_release (&frame_table_lock);
+
 }
 
 /* Removes given frame table entry from the frame table. */
@@ -64,23 +65,21 @@ remove_frame (void *frame) {
 
   struct frame_entry *temp_entry = search_elem (frame);
   hash_delete (frame_table, &temp_entry->elem);
-  palloc_free_page (frame->frame_address); // necessary??
-  free (temp_entry); 
 
+  free (temp_entry); 
   lock_release (&frame_table_lock);
 }
 
 /* Allocates a frame for the given page. */
 void *
 frame_alloc (enum palloc_flags flags) {
-  void *kpage = palloc_get_page (PAL_USER | flags);
-  if (kpage == NULL) {
-    PANIC ("Page eviction not implemented.");
-    /* TODO: evict page & palloc again */
+  void *f = palloc_get_page (PAL_USER | flags);
+  if (f == NULL) {
+    ASSERT(false);
   }
-
   add_frame (f);
   return f;
+
 }
 
 /* Frees all resources used by frame table entry
